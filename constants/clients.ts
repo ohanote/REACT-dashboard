e2be8d0d01ecb89aaddeff39f3ffbd772d2f2a94
ohanote <s.de.souza.davi@gmail.com>
2024-04-10 13:30:50 -0300

    Added Invoices Card

---
+++ constants/clients.ts
@@ -0,0 +1,44 @@
+const CLIENTS = [
+  {
+    imageURL: "https://source.unsplash.com/random/900×700/?profile",
+    name: "Koen Paulie",
+    id: "1",
+    date: "31/04/2024",
+    Amount: "$ 3.230,2",
+    productId: "001423",
+    status: "Paid",
+    option: "More"
+  },
+  {
+    imageURL: "https://source.unsplash.com/random/900×700/?profile-woman",
+    name: "Arlene McCoy",
+    id: "1",
+    date: "28/04/2024",
+    Amount: "$ 3.145,2",
+    productId: "001817",
+    status: "Unpaid",
+    option: "More"
+  },
+  {
+    imageURL: "https://source.unsplash.com/random/900×700/?profile-ginger",
+    name: "Jacob Jones",
+    id: "1",
+    date: "12/04/2024",
+    Amount: "$ 3.817,2",
+    productId: "038153",
+    status: "Paid",
+    option: "More"
+  },
+  {
+    imageURL: "https://source.unsplash.com/random/900×700/?profile-man",
+    name: "Marvin Butcher",
+    id: "1",
+    date: "17/04/2024",
+    Amount: "$ 6.176,2",
+    productId: "006917",
+    status: "Paid",
+    option: "More"
+  },
+]
+
+export default CLIENTS
--- src/Components/Dashboard.tsx
+++ src/Components/Dashboard.tsx
@@ -2,6 +2,7 @@ import BarsCharts from "./BarsCharts";
 import CustomPieCharts from "./CustomPieCharts";
 import LinesCharts from "./LinesCharts";
 import MainCard from "./MainCard";
+import Invoices from "./Invoices";
 import IconSearch from "../../src/assets/icon-search.svg"
 import IconDarkMode from "../../src/assets/icon-dark-mode.svg"
 import IconNotification from "../../src/assets/icon-notification.svg"
@@ -9,6 +10,7 @@ import IconSales from "../../src/assets/icon-sales.svg"
 import IconProfit from "../../src/assets/icon-profit.svg"
 import IconAI from "../../src/assets/icon-ai.svg"
 import IconActivity from "../../src/assets/icon-activity.svg"
+import IconInvoice from "../../src/assets/icon-invoice.svg"
 
 const Dashboard = () => {
   return (
@@ -65,21 +67,21 @@ const Dashboard = () => {
         <BarsCharts
           title="Sales Report"
           subtitle="Earnings"
-          bgColor="bg-zinc-600"
+          bgColor="bg-zinc-800"
           icon={IconSales}
           textColor="#5eead4"
         />
         <LinesCharts
           title="Analytical AI"
           subtitle="April 2024"
-          bgColor="bg-zinc-600"
+          bgColor="bg-zinc-800"
           icon={IconAI}
           textColor="#5eead4"
         />
-        <div className="card span-2">teste</div>
+        <Invoices title={"Invoices"} icon={IconInvoice} bgColor={""} textColor={""} />
         <CustomPieCharts
           title="Activity"
-          bgColor="bg-zinc-600"
+          bgColor="bg-zinc-800"
           icon={IconActivity}
           textColor="#fff"
         />
