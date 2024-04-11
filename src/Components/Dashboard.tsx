7ad47fdd89eb6208f8cdb2851c0d767341e71c7a
ohanote <s.de.souza.davi@gmail.com>
2024-04-11 13:38:44 -0300

    Finished the Dark/Light Mode Feature Added hover animations to menu items butons

--- src/Components/Dashboard.tsx
+++ src/Components/Dashboard.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import BarsCharts from "./BarsCharts";
 import CustomPieCharts from "./CustomPieCharts";
 import LinesCharts from "./LinesCharts";
@@ -5,6 +6,7 @@ import MainCard from "./MainCard";
 import Invoices from "./Invoices";
 import IconSearch from "../../src/assets/icon-search.svg";
 import IconDarkMode from "../../src/assets/icon-dark-mode.svg";
+import IconLightMode from "../../src/assets/icon-light-mode.svg";
 import IconNotification from "../../src/assets/icon-notification.svg";
 import IconSales from "../../src/assets/icon-sales.svg";
 import IconProfit from "../../src/assets/icon-profit.svg";
@@ -13,6 +15,20 @@ import IconActivity from "../../src/assets/icon-activity.svg";
 import IconInvoice from "../../src/assets/icon-invoice.svg";
 
 const Dashboard = () => {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  const handleThemeSwitch = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
   return (
     <section className="flex flex-col p-16 border-l-zinc-300 dark:border-l-zinc-700 border-l-2 h-[100vh]">
       <header className="items-center justify-between flex pb-8">
@@ -30,11 +46,26 @@ const Dashboard = () => {
           />
         </div>
         <div className="flex gap-2">
-          <div className="border-2 rounded-md border-zinc-700 bg-zinc-950 p-2 w-12 h-12 flex items-center justify-center hover:bg-zinc-700 cursor-pointer transition-all">
-            <IconDarkMode width={24} height={24} fill="white" />
+          <div
+            className="border-2 rounded-md border-zinc-300 bg-white dark:border-zinc-700 dark:bg-zinc-950 p-2 w-12 h-12 flex items-center justify-center hover:bg-zinc-300 dark:hover:bg-zinc-700 cursor-pointer transition-all"
+            onClick={handleThemeSwitch}
+          >
+            {theme === "light" ? (
+              <IconDarkMode
+                width={24}
+                height={24}
+                className="fill-zinc-800 dark:fill-white"
+              />
+            ) : (
+              <IconLightMode
+                width={24}
+                height={24}
+                className="fill-zinc-800 dark:fill-white"
+              />
+            )}
           </div>
-          <div className="border-2 rounded-md border-zinc-700 bg-zinc-950 p-2 w-12 h-12 flex items-center justify-center hover:bg-zinc-700 cursor-pointer transition-all">
-            <IconNotification width={24} height={24} fill="white" />
+          <div className="border-2 rounded-md border-zinc-300 bg-white dark:border-zinc-700 dark:bg-zinc-950 p-2 w-12 h-12 flex items-center justify-center hover:bg-zinc-300 dark:hover:bg-zinc-700 cursor-pointer transition-all">
+            <IconNotification width={24} height={24}className="fill-zinc-800 dark:fill-white" />
           </div>
           <div className="border-2 rounded-md border-zinc-700 bg-zinc-950 overflow-hidden w-12 h-12 flex items-center justify-center cursor-pointer transition-all">
             <img
@@ -53,33 +84,63 @@ const Dashboard = () => {
           mainText="$12 895.5"
           description="+14% Since last week"
           bgColor="dark:bg-teal-800 bg-teal-300"
-          icon={<IconProfit className="fill-none stroke-zinc-700 dark:stroke-white" width={36} height={36}/>}
+          icon={
+            <IconProfit
+              className="fill-none stroke-zinc-700 dark:stroke-white"
+              width={36}
+              height={36}
+            />
+          }
         />
         <BarsCharts
           title="Sales Report"
           subtitle="Earnings"
           bgColor="dark:bg-zinc-800 bg-zinc-300"
-          icon={<IconSales className="stroke-none fill-zinc-700 dark:fill-white" width={36} height={36}/>}
-          textColor="#5eead4"
+          icon={
+            <IconSales
+              className="stroke-none fill-zinc-700 dark:fill-white"
+              width={36}
+              height={36}
+            />
+          }
+          textColor={theme === "light" ? "#27272a" : "#5eead4"}
         />
         <LinesCharts
           title="Analytical AI"
           subtitle="April 2024"
           bgColor="dark:bg-zinc-800 bg-zinc-300"
-          icon={<IconAI className="fill-none stroke-zinc-700 dark:stroke-white" width={36} height={36}/>}
-          textColor="#5eead4"
+          icon={
+            <IconAI
+              className="fill-none stroke-zinc-700 dark:stroke-white"
+              width={36}
+              height={36}
+            />
+          }
+          textColor={theme === "light" ? "#27272a" : "#5eead4"}
         />
         <Invoices
           title={"Invoices"}
-          icon={<IconInvoice className="stroke-none fill-zinc-700 dark:fill-white" width={36} height={36}/>}
+          icon={
+            <IconInvoice
+              className="stroke-none fill-zinc-700 dark:fill-white"
+              width={36}
+              height={36}
+            />
+          }
           bgColor={""}
           textColor={""}
         />
         <CustomPieCharts
           title="Activity"
           bgColor="dark:bg-zinc-800 bg-zinc-300"
-          icon={<IconActivity className="stroke-none fill-zinc-700 dark:fill-white" width={36} height={36} />}
-          textColor="#fff"
+          icon={
+            <IconActivity
+              className="stroke-none fill-zinc-700 dark:fill-white"
+              width={36}
+              height={36}
+            />
+          }
+          textColor={theme === "light" ? "#27272a" : "#5eead4"}
         />
       </main>
     </section>
